9f92e19fd28b3918281cba1f653988731acf6f24
nameless <a-arsenij@yandex.ru>
2021-09-01 04:13:49 +0300

    Slow&fast increments

--- src/components/HooksTests/View2.js
+++ src/components/HooksTests/View2.js
@@ -1,63 +1,65 @@
-import React, { useState } from "react";
-import { Button } from "../Controls/Button";
-import { makeStyles } from "@material-ui/core/styles";
+import React, {useMemo, useState} from "react";
+import {Button} from "../Controls/Button";
+import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%",
-    minHeight: 500,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-  },
-  buttonsWrapper: {
-    marginTop: 10,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    marginBottom: 15,
-  },
+    root: {
+        width: "100%",
+        minHeight: 500,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+    },
+    buttonsWrapper: {
+        marginTop: 10,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+        marginBottom: 15,
+    },
 }));
 
 export const View2 = () => {
-  const classes = useStyles();
-  const [slowCounter, setSlowCounter] = useState(0);
-  const [fastCounter, setFastCounter] = useState(0);
+    const classes = useStyles();
+    const [slowCounter, setSlowCounter] = useState(0);
+    const [fastCounter, setFastCounter] = useState(0);
 
-  const slowIncrement = () => {
+    const slowIncrement = () => {
+        setSlowCounter(prev => prev + 1);
+    };
 
-  };
+    const fastIncrement = () => {
+        setFastCounter(prev => prev + 1);
+    };
 
-  const fastIncrement = () => {
+    const calculatedValue = useMemo(() => {
+            for (let i = 0; i < 1000000000; i++) {
+                //do something
+            }
+            return slowCounter;
+        },
+        [slowCounter]
+    );
 
-  };
-
-  const calculate = () => {
-    for (let i = 0; i < 1000000000; i++) {
-      //do something
-    }
-    return slowCounter;
-  };
-
-  return (
-    <div className={classes.root}>
-      <div>
-        <h4>{calculate()}</h4>
-        <div className={classes.buttonsWrapper}>
-          <Button onClick={slowIncrement} className={classes.button}>
-            Slow Increment
-          </Button>
+    return (
+        <div className={classes.root}>
+            <div>
+                <h4>{calculatedValue}</h4>
+                <div className={classes.buttonsWrapper}>
+                    <Button onClick={slowIncrement} className={classes.button}>
+                        Slow Increment
+                    </Button>
+                </div>
+                <h4>{fastCounter}</h4>
+                <div className={classes.buttonsWrapper}>
+                    <Button onClick={fastIncrement} className={classes.button}>
+                        Fast Increment
+                    </Button>
+                </div>
+            </div>
         </div>
-        <h4>{fastCounter}</h4>
-        <div className={classes.buttonsWrapper}>
-          <Button onClick={fastIncrement} className={classes.button}>
-            Fast Increment
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
